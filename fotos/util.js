7583bde95790af94070c679fad5c9e334eaed4a0
fvortmann <fvortmann@hotmail.com>
2025-09-14 05:10:43 -0400

    add

--- fotos/util.js
+++ fotos/util.js
@@ -24,10 +24,42 @@ function load() {
 			const image = evento[0]
 			const desc = evento[1]
 			const path = image.substring(0,image.lastIndexOf("/")+1)
-			table += `<tr><td width="400px"><a href="index.htm?path=${path}"><img src="${image}" width="400px" height="200px"></a></td><td width="70px">&nbsp;</td>`
+			table += `<tr><td width="400px"><a href="evento.htm?path=${path}"><img src="${image}" width="400px" height="200px"></a></td><td width="70px">&nbsp;</td>`
 			table += `<td align="center">${desc}</td><td width="70px">&nbsp;</td></tr><tr><td height="20px" colspan="4"></td></tr>`
 	  });
 	  div.innerHTML = table
-	});
-	
+	});	
 }
+
+function listarImagens() {
+
+}
+
+async function preCarregarImagens() {
+  for (let i = 0; i <= 5; i++) {
+    const urlDaImagem = `caminho/para/sua/imagem${i}.jpg`;
+    try {
+      // Cria uma promessa para tentar carregar a imagem
+      const promessaDeCarregamento = new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = () => reject();
+        img.src = urlDaImagem;
+      });
+
+      // Aguarda o resultado da promessa
+      await promessaDeCarregamento;
+      console.log(`Imagem ${i} carregada com sucesso!`);
+
+    } catch (error) {
+      // Se houver um erro, a promessa é rejeitada e o 'catch' é executado
+      console.log(`Imagem ${i} não encontrada. Parando o pré-carregamento.`);
+      // O 'return' em uma função async dentro de um loop tem o mesmo efeito
+      // que o 'break'. Ele para a execução da função.
+      return; 
+    }
+  }
+}
+
+// Chama a função para iniciar o pré-carregamento
+preCarregarImagens();
